8ffe359bb2a14888083a97ebad793241205ee75b
freedomzzq <freedomzzq@gmail.com>
2016-04-17 22:43:54 +0800

    begin making setting

--- app/src/main/java/studio/androiddev/puzzle/activity/SettingActivity.java
+++ app/src/main/java/studio/androiddev/puzzle/activity/SettingActivity.java
@@ -3,33 +3,39 @@ package studio.androiddev.puzzle.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import studio.androiddev.puzzle.R;
 
 public class SettingActivity extends BaseActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+
     }
 
-    public static void actionStart(Context context){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static void actionStart(Context context) {
         Intent intent = new Intent(context, SettingActivity.class);
         context.startActivity(intent);
     }
